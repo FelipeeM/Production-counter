fed40a8fe2ee64233e87f2e94d9fb09681a3e603
FelipeeM <36524565+FelipeeM@users.noreply.github.com>
2019-08-16 17:25:22 -0300

    Update Nrf_Count_Receptor.ino
    Bug no recebimento dos dados, M2 = M1

--- Nrf_Count_Receptor.ino
+++ Nrf_Count_Receptor.ino
@@ -1,36 +1,133 @@
+#include <Arduino.h>
 #include <SPI.h>
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
-const byte address[6] = "00001";
+const byte address[][6] = {"00001","00002" };
 boolean button_state = 0;
-int led_pin = 3;
-int contagem;
+//Pinos
+
+const int reset = 3;
+int contagem_M1 = 0;
+int contagem_M2 = 0;
+int check_M1;
+int check_M2;
+int check;
 
 void setup() {
-  pinMode(6, OUTPUT);
+  
   Serial.begin(9600);
+ 
+  //radio
   radio.begin();
-  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+ 
+  radio.openWritingPipe(address[1]);
+  radio.openReadingPipe(1, address[0]);   //Setting the address at which we will receive the data
+  
   radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
-  radio.startListening();              //This sets the module as receiver
+ 
+ //botoes
+  pinMode(reset, OUTPUT);
+  digitalWrite(reset, HIGH);
+
 }
+void Receber();
+void Resetar();
+void Maquina_1();
+void Maquina_2();
+
 void loop(){
 
-  Receber();
+
+  radio.startListening();              // ler informaçoes do outro pipe 
+
   
+  Receber();
+  Maquina_1();
+  Maquina_2();
+  //Resetar();
+  delay(200);
+
 }
+ 
+
 
 void Receber() {
 
 
-  if (radio.available()) { //verifica se a conexão esta habilitada
+  while(radio.available()) { //verifica se a conexão esta habilitada
+    //Serial.println("conexao ativa");
+    radio.read(&contagem_M1, sizeof(int));
+    Serial.print("contagem_M1: ");
+    Serial.println(contagem_M1);
 
-   
-    radio.read(&contagem, sizeof(int));    //Reading the data
+    delay(100);
 
-    Serial.println(contagem);
-
+    radio.read(&contagem_M2, sizeof(int));
+    Serial.print("contagem_M2: ");
+    Serial.println(contagem_M2);
+    
     delay(100);
   }
   
+  delay(50);
+
 }
+
+void Resetar(){
+     // Serial.println(digitalRead(reset));
+      if (digitalRead(reset) == 0){
+
+        radio.stopListening(); // parar de ouvir para enviar informaçoes
+
+        Serial.println("Envio resetar");
+        check = radio.write(&reset, sizeof(int));
+        Serial.print("checagem: ");
+        Serial.println(digitalRead(reset));
+
+        contagem_M1 = 0;
+        contagem_M2 = 0;
+        check_M1 = 0;
+        check_M2 = 0;
+
+              
+      }
+
+
+}
+
+void Maquina_1(){
+
+    
+        
+   if (contagem_M1 > check_M1)
+    {
+       Serial.println(" ");
+       Serial.println("Maquina 1");
+       Serial.print("Numero de ciclos: ");
+       Serial.println(contagem_M1);
+       Serial.println(" ");
+
+       check_M1 = contagem_M1; 
+
+    }
+    
+}
+
+void Maquina_2(){
+
+         
+        
+   if (contagem_M2 > check_M2)
+    {
+       Serial.println(" ");
+       Serial.println("Maquina 2");
+       Serial.print("Numero de ciclos: ");
+       Serial.println(contagem_M2);
+       Serial.println(" ");
+
+       check_M2 = contagem_M2; 
+
+    }
+    
+}
+
